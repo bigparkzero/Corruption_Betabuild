2b378806c79e465acd5b7aadf17a1625a14404f8
bigparkzero <ljjok1234@gmail.com>
2024-09-27 17:44:54 +0900

    chkwhdwqlfewm

---
+++ Assets/waeawkdbw.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class waeawkdbw : MonoBehaviour
+{
+    public GameObject cawkjugcw;
+    private void Update()
+    {
+        cawkjugcw.SetActive(true);
+    }
+    private void OnEnable()
+    {
+        cawkjugcw.SetActive(true);
+    }
+    private void OnDisable()
+    {
+        cawkjugcw.SetActive(true);
+    }
+}
